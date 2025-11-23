2face0a719ad55ba7510d795f2c66cb0e304c470
f0lderoff <vladkozin2020@gmail.com>
2025-11-23 20:47:58 +0300

    Практическая 14

---
+++ Pracc_14/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("### ЗАДАНИЕ 1 ###");
+            Instrument[] band = { new Guitar(), new Drum(), new Instrument() };
+            foreach (var i in band)
+                i.Play();
+
+            Console.WriteLine("### ЗАДАНИЕ 2 ###");
+            Message[] messages = { new Email(), new SMS(), new Message() };
+            foreach (var m in messages)
+                m.Send();
+
+            Console.WriteLine("### ЗАДАНИЕ 3 ###");
+            Food[] lunch = { new Pizza(), new Burger(), new Food() };
+            foreach (var f in lunch)
+                f.Eat();
+
+            Console.WriteLine("### ЗАДАНИЕ 4 ###");
+            Shape[] shapes = { new Circle(), new Triangle() };
+            foreach (var s in shapes)
+                Console.WriteLine(s.Draw());
+
+            Console.WriteLine("### ЗАДАНИЕ 5 ###");
+            Player[] team = { new Warrior(), new Archer(), new Player() };
+            foreach (var p in team)
+                p.Attack();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Pracc_14/classes.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Instrument
+    {
+        public virtual void Play() => Console.WriteLine("Играет музыка...");
+    }
+
+    class Guitar : Instrument
+    {
+        public override void Play() => Console.WriteLine("Брень-брень!");
+    }
+
+    class Drum : Instrument
+    {
+        public override void Play() => Console.WriteLine("Бум-бум!");
+    }
+    class Message
+    {
+        public virtual void Send() => Console.WriteLine("Сообщение отправлено.");
+    }
+
+    class Email : Message
+    {
+        public override void Send() => Console.WriteLine("Email отправлен!");
+    }
+
+    class SMS : Message
+    {
+        public override void Send() => Console.WriteLine("SMS отправлено!");
+    }
+
+    class Food
+    {
+        public virtual void Eat() => Console.WriteLine("Едим...");
+    }
+
+    class Pizza : Food
+    {
+        public override void Eat() => Console.WriteLine("Едим пиццу!");
+    }
+
+    class Burger : Food
+    {
+        public override void Eat() => Console.WriteLine("Едим бургер!");
+    }
+
+    abstract class Shape
+    {
+        public abstract string Draw();
+    }
+
+    class Circle : Shape
+    {
+        public override string Draw() => "Рисую круг";
+    }
+
+    class Triangle : Shape
+    {
+        public override string Draw() => "Рисую треугольник";
+    }
+
+    class Player
+    {
+        public virtual void Attack() => Console.WriteLine("Атакую!");
+    }
+
+    class Warrior : Player
+    {
+        public override void Attack() => Console.WriteLine("Рублю мечом!");
+    }
+
+    class Archer : Player
+    {
+        public override void Attack() => Console.WriteLine("Стреляю из лука!");
+    }
+}
